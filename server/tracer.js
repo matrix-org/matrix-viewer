e20528eb79b387c5d7f88536ad821dcffcb04876
Eric Eastwood <contact@ericeastwood.com>
2022-06-16 22:08:14 -0500

    Ignore spans from static assets

--- server/tracer.js
+++ server/tracer.js
@@ -14,8 +14,9 @@ const { OTTracePropagator } = require('@opentelemetry/propagator-ot-trace');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-// This is from the official guide but when uncommenting, this throws `Error:
-// @opentelemetry/api: Attempted duplicate registration of API: trace`.
+// This is from the official guide but when uncommenting, this throws:
+//  - `Error: @opentelemetry/api: Attempted duplicate registration of API: trace`.
+//  - `Error: @opentelemetry/api: Attempted duplicate registration of API: propagation`
 //
 // (Diagnostics) For troubleshooting, set the log level to DiagLogLevel.DEBUG.
 //
@@ -31,9 +32,13 @@ const provider = new BasicTracerProvider({
     [SemanticResourceAttributes.SERVICE_NAME]: 'matrix-public-archive',
   }),
 });
-// export spans to console (useful for debugging)
+// Export spans to console (useful for debugging).
 // provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-// export spans to opentelemetry collector
+
+// Export spans to opentelemetry collector.
+// Use this for immediate span submission
+//provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+// Otherwise, we should just use the more performant batched processor
 provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
 provider.register({ propagator: new OTTracePropagator() });
@@ -41,7 +46,29 @@ provider.register({ propagator: new OTTracePropagator() });
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: exporter,
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-http': {
+        // Docs:
+        // https://github.com/open-telemetry/opentelemetry-js/tree/51afd54bd63e46d5d530266761144c7be2f6b3a7/experimental/packages/opentelemetry-instrumentation-http
+        //
+        // This is the place to ignore root level spans for Express routes. My
+        // first guess would be in `ignoreLayers` in
+        // `@opentelemetry/instrumentation-express` but that's not the case, see
+        // https://github.com/open-telemetry/opentelemetry-js-contrib/issues/1034#issuecomment-1158435392
+        ignoreIncomingPaths: [
+          (url) => {
+            // Ignore spans from static assets. Ideally, all of the assets would
+            // all be served under `/static/` so we could ignore that way
+            // instead. In Hydrogen, this is tracked by
+            // https://github.com/vector-im/hydrogen-web/issues/757
+            const isStaticAsset = url.match(/\.(css|js|svg|woff2)$/);
+            return isStaticAsset;
+          },
+        ],
+      },
+    }),
+  ],
 });
 
 sdk
