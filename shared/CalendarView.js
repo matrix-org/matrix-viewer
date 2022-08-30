9b2c215a5312e3ee4db86497897018a29d5ac529
Eric Eastwood <contact@ericeastwood.com>
2022-08-30 01:48:57 -0500

    Fix typo

--- shared/CalendarView.js
+++ shared/CalendarView.js
@@ -66,7 +66,7 @@ class CalendarView extends TemplateView {
                   month: 'long',
                   timeZone: 'UTC',
                 }),
-                // Drowndown arrow
+                // Dropdown arrow
                 t.svg(
                   {
                     xmlns: 'http://www.w3.org/2000/svg',
