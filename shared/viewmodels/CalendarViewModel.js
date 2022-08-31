d9c0f66c9daddac7fdaba4272388def93fb2de36
Eric Eastwood <contact@ericeastwood.com>
2022-08-30 19:10:57 -0500

    Use `DisabledComposerView` instead of no composer to add message (#54)
    Use `DisabledComposerView` instead of no composer to add message:
    
    > You're viewing an archive of events from 2022-02-08. Use a [Matrix client](https://matrix.to/#/!HBehERstyQBxyJDLfR:my.synapse.server) to start chatting in this room.
    
    Also refactors some of the `views` and `viewmodels` out to their own files

---
+++ shared/viewmodels/CalendarViewModel.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { ViewModel } = require('hydrogen-view-sdk');
+
+const assert = require('matrix-public-archive-shared/lib/assert');
+const MatrixPublicArchiveURLCreator = require('matrix-public-archive-shared/lib/url-creator');
+
+class CalendarViewModel extends ViewModel {
+  constructor(options) {
+    super(options);
+    const { activeDate, calendarDate, room, basePath } = options;
+    assert(activeDate);
+    assert(calendarDate);
+    assert(room);
+    assert(basePath);
+
+    this._activeDate = activeDate;
+    this._calendarDate = calendarDate;
+    this._room = room;
+    this._matrixPublicArchiveURLCreator = new MatrixPublicArchiveURLCreator(basePath);
+  }
+
+  get activeDate() {
+    return this._activeDate;
+  }
+
+  get calendarDate() {
+    return this._calendarDate;
+  }
+
+  archiveUrlForDate(date) {
+    return this._matrixPublicArchiveURLCreator.archiveUrlForDate(this._room.id, date);
+  }
+
+  prevMonth() {
+    const prevMonthDate = new Date(this._calendarDate);
+    prevMonthDate.setUTCMonth(this._calendarDate.getUTCMonth() - 1);
+    this._calendarDate = prevMonthDate;
+    this.emitChange('calendarDate');
+  }
+
+  nextMonth() {
+    const nextMonthDate = new Date(this._calendarDate);
+    nextMonthDate.setUTCMonth(this._calendarDate.getUTCMonth() + 1);
+    console.log('nextMonthDate', nextMonthDate);
+    this._calendarDate = nextMonthDate;
+    this.emitChange('calendarDate');
+  }
+
+  onMonthInputChange(e) {
+    this._calendarDate = e.target.valueAsDate;
+    this.emitChange('calendarDate');
+  }
+
+  onYearFallbackSelectChange(e) {
+    const selectedDate = new Date(this._calendarDate);
+    selectedDate.setUTCFullYear(e.target.value);
+    this._calendarDate = selectedDate;
+    this.emitChange('calendarDate');
+  }
+}
+
+module.exports = CalendarViewModel;
