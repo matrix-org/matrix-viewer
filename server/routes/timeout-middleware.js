matrix-org/matrix-viewer

commit 127d416e6ae73cff4a630007472986ea8616b775

Room directory landing page v1 (#61)
Part of https://github.com/matrix-org/matrix-public-archive/issues/6

server/routes/timeout-middleware.js
@@ -48,8 +48,8 @@ async function timeoutMiddleware(req, res, next) {
       humanReadableSpans = [noTracingDataAvailableItem];
     }
 
-    const hydrogenStylesUrl = urlJoin(basePath, 'hydrogen-styles.css');
-    const stylesUrl = urlJoin(basePath, 'styles.css');
+    const hydrogenStylesUrl = urlJoin(basePath, '/css/hydrogen-styles.css');
+    const stylesUrl = urlJoin(basePath, '/css/styles.css');
 
     const pageHtml = `
     <!doctype html>

.github/workflows/containerize.yaml
@@ -73,6 +73,9 @@ jobs:
           file: 'Dockerfile'
           tags: ${{ steps.meta.outputs.tags }}
           labels: ${{ steps.meta.outputs.labels }}
+          build-args: |
+            GITHUB_SHA=${{ github.sha }}
+            GITHUB_REF=${{ github.ref_name }}
 
   # Just make sure the container can start-up and responds to the health check
   test-image:

Dockerfile
@@ -4,6 +4,12 @@
 #  - https://github.com/npm/cli/issues/4769
 FROM node:16.14.2-buster-slim
 
+# Pass through some GitHub CI variables which we use in the build
+ARG GITHUB_SHA
+ENV GITHUB_SHA=$GITHUB_SHA
+ARG GITHUB_REF
+ENV GITHUB_REF=$GITHUB_REF
+
 RUN mkdir -p /app
 
 WORKDIR /app
@@ -18,9 +24,9 @@ RUN npm install
 
 # Copy what we need for the client-side build
 COPY config /app/config/
+COPY build /app/build/
 COPY public /app/public/
 COPY shared /app/shared/
-COPY vite.config.js /app/
 # Build the client-side bundle
 RUN npm run build
 

build/build-client-scripts.js
@@ -1,0 +1,37 @@
+'use strict';
+
+// A way to build multiple Vite entrypoints
+
+// We can remove this once Vite supports multiple entrypoints and
+// https://github.com/vitejs/vite/pull/7047 lands. We can migrate back to a
+// normal `vite.config.js` and what we had before.
+//
+// Related issues:
+//  - https://github.com/vitejs/vite/issues/4530
+//  - https://github.com/vitejs/vite/discussions/1736
+
+const path = require('path');
+const vite = require('vite');
+const mergeOptions = require('merge-options');
+
+const generateViteConfigForEntryPoint = require('./generate-vite-config-for-entry-point');
+
+const entryPoints = [
+  path.resolve(__dirname, '../public/js/entry-client-hydrogen.js'),
+  path.resolve(__dirname, '../public/js/entry-client-room-directory.js'),
+];
+
+async function buildClientScripts(extraConfig = {}) {
+  for (const entryPoint of entryPoints) {
+    // Build the client-side JavaScript bundle when we see any changes
+    const viteConfig = mergeOptions(
+      generateViteConfigForEntryPoint(entryPoint),
+      // Since we're building multiple entryPoints, we don't want it to clear out for each one
+      { build: { emptyOutDir: false } },
+      extraConfig
+    );
+    await vite.build(viteConfig);
+  }
+}
+
+module.exports = buildClientScripts;

build/build-client.js
@@ -1,0 +1,10 @@
+'use strict';
+
+const buildClientScripts = require('./build-client-scripts');
+const writeVersionFiles = require('./write-version-files');
+
+async function build(extraConfig) {
+  await Promise.all([writeVersionFiles(), buildClientScripts(extraConfig)]);
+}
+
+module.exports = build;

build/do-client-build.js
@@ -1,0 +1,8 @@
+'use strict';
+
+// This is just a callable from the commandline version of `build-client.js`. So
+// that we can run the build from an npm script.
+
+const buildClient = require('./build-client');
+
+buildClient();

build/generate-vite-config-for-entry-point.js
@@ -1,0 +1,44 @@
+// vite.config.js
+'use strict';
+
+const path = require('path');
+const { defineConfig } = require('vite');
+
+function generateViteConfigForEntryPoint(entryPoint) {
+  const entryPointName = path.basename(entryPoint, '.js');
+
+  return defineConfig({
+    // We have to specify this otherwise Vite will override NODE_ENV as
+    // `production` when we start the server and watch build in our `start-dev.js`.
+    mode: process.env.NODE_ENV || 'dev',
+
+    //root: './',
+    //base: './',
+    outDir: './dist',
+    // optimizeDeps: {
+    //   include: ['matrix-public-archive-shared'],
+    // },
+    resolve: {
+      alias: {
+        // The `file:` packages don't seem resolve correctly so let's add an alias as well
+        // See https://css-tricks.com/adding-vite-to-your-existing-web-app/#aa-aliases
+        'matrix-public-archive-shared': path.resolve(__dirname, '../shared'),
+      },
+    },
+    build: {
+      // Fix `Error: 'default' is not exported by ...` when importin CommonJS files,
+      // see https://github.com/vitejs/vite/issues/2679
+      commonjsOptions: { include: [] },
+
+      lib: {
+        entry: entryPoint,
+        //formats: ['cjs'],
+        name: entryPointName,
+        fileName: (format) => `${entryPointName}.${format}.js`,
+      },
+      rollupOptions: {},
+    },
+  });
+}
+
+module.exports = generateViteConfigForEntryPoint;

build/write-version-files.js
@@ -1,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const { mkdir, writeFile } = require('fs').promises;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function mkdirp(path) {
+  try {
+    await mkdir(path, { recursive: true });
+  } catch (err) {
+    console.log('mkdirp err', err);
+    // no-op, the directory is already created
+  }
+}
+
+async function writeVersionFiles() {
+  let commit;
+  let branch;
+  try {
+    commit = (await exec(`git rev-parse HEAD`)).stdout;
+    branch = (await exec(`git rev-parse --abbrev-ref HEAD`)).stdout;
+  } catch (err) {
+    console.log(
+      `Failed to use \`git\` to find the commit and branch.` +
+        ` Falling back to using environment variables assuming we're running in GitHub CI. The error encountered:`,
+      err
+    );
+
+    // Pull these values from environment variables provided by GitHub CI
+    commit = process.env.GITHUB_SHA;
+    branch = process.env.GITHUB_REF;
+  }
+
+  if (!commit || !branch) {
+    throw new Error(
+      `Unable to get a suitable commit=${commit} or branch=${branch} while writing version files`
+    );
+  }
+
+  await mkdirp(path.join(__dirname, '../dist/'));
+  await writeFile(path.join(__dirname, '../dist/GIT_COMMIT'), commit);
+  await writeFile(path.join(__dirname, '../dist/VERSION'), branch);
+  await writeFile(path.join(__dirname, '../dist/VERSION_DATE'), new Date().toISOString());
+}
+
+module.exports = writeVersionFiles;

commit b8062b16a20ec4a54fd11bd9f61d74652bfc50b1

Fix wrong path to Hydrogen styles on timeout error page (#83)
Regressed in https://github.com/matrix-org/matrix-public-archive/pull/61 where we tried to serve this under `/css/hydrogen-styles.css` but it doesn't work because all of the image and font references in the CSS file expect it to be at the domain root so just reverted back to serving at the root `/`.

server/routes/timeout-middleware.js
@@ -48,7 +48,7 @@ async function timeoutMiddleware(req, res, next) {
       humanReadableSpans = [noTracingDataAvailableItem];
     }
 
-    const hydrogenStylesUrl = urlJoin(basePath, '/css/hydrogen-styles.css');
+    const hydrogenStylesUrl = urlJoin(basePath, '/hydrogen-styles.css');
     const stylesUrl = urlJoin(basePath, '/css/styles.css');
 
     const pageHtml = `