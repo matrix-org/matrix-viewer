7c0b52e00d6c9677605a1d73950ecbeeca3479c2
Eric Eastwood <contact@ericeastwood.com>
2023-04-25 03:02:06 -0500

    Smarter watching

--- server/lib/get-dependencies-for-entry-point-name.js
+++ server/lib/get-dependencies-for-entry-point-name.js
@@ -10,6 +10,7 @@ function getManifest() {
   if (_manifest) {
     return _manifest;
   }
+
   // We have to disable this because it's built via the Vite client build.
   // eslint-disable-next-line n/no-missing-require, n/no-unpublished-require
   _manifest = require('../../dist/manifest.json');
