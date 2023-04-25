a41c2429d6f9d8bf2196c173d0c62462fd1413f8
Eric Eastwood <contact@ericeastwood.com>
2023-04-25 00:44:06 -0500

    Fix caps

--- server/hydrogen-render/render-hydrogen-to-string.js
+++ server/hydrogen-render/render-hydrogen-to-string.js
@@ -10,7 +10,7 @@ const assert = require('assert');
 const RethrownError = require('../lib/rethrown-error');
 const runInChildProcess = require('../child-process-runner/run-in-child-process');
 
-const resolvedREnderHydrogenToStringUnsafeScriptPath = require.resolve(
+const resolvedRenderHydrogenToStringUnsafeScriptPath = require.resolve(
   './render-hydrogen-to-string-unsafe'
 );
 
@@ -44,7 +44,7 @@ async function renderHydrogenToString(renderOptions) {
     // we receive the SSR results. We don't want Hydrogen to keep running after we
     // get our initial rendered HTML.
     const hydrogenHtmlOutput = await runInChildProcess(
-      resolvedREnderHydrogenToStringUnsafeScriptPath,
+      resolvedRenderHydrogenToStringUnsafeScriptPath,
       renderOptions,
       {
         timeout: RENDER_TIMEOUT,
