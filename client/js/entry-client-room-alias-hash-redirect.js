a7cd14a4822b4934a62deb0342bd3ba49e8f2de6
Eric Eastwood <contact@ericeastwood.com>
2023-04-24 23:18:27 -0500

    Fix lint

--- client/js/entry-client-room-alias-hash-redirect.js
+++ client/js/entry-client-room-alias-hash-redirect.js
@@ -3,6 +3,10 @@ import MatrixPublicArchiveURLCreator from 'matrix-public-archive-shared/lib/url-
 import redirectIfRoomAliasInHash from 'matrix-public-archive-shared/lib/redirect-if-room-alias-in-hash';
 
 // Assets
+// We have to disable no-missing-require lint because it doesn't take into
+// account `package.json`. `exports`, see
+// https://github.com/mysticatea/eslint-plugin-node/issues/255
+// eslint-disable-next-line node/no-missing-import
 import 'hydrogen-view-sdk/assets/theme-element-light.css';
 import '../css/styles.css';
 
--- client/js/entry-client-room-directory.js
+++ client/js/entry-client-room-directory.js
@@ -1,6 +1,10 @@
 import 'matrix-public-archive-shared/room-directory-vm-render-script';
 
 // Assets
+// We have to disable no-missing-require lint because it doesn't take into
+// account `package.json`. `exports`, see
+// https://github.com/mysticatea/eslint-plugin-node/issues/255
+// eslint-disable-next-line node/no-missing-import
 import 'hydrogen-view-sdk/assets/theme-element-light.css';
 import '../css/styles.css';
 import '../css/room-directory.css';
