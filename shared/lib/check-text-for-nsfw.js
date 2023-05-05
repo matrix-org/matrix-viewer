dd1d712990bbe0f724257020a71d657b16914a7c
Eric Eastwood <contact@ericeastwood.com>
2023-05-05 15:31:49 -0500

    Describe very basic

--- shared/lib/check-text-for-nsfw.js
+++ shared/lib/check-text-for-nsfw.js
@@ -3,6 +3,8 @@
 const NSFW_WORDS = ['nsfw', 'porn', 'nudes', 'sex', '18+'];
 const NSFW_REGEXES = NSFW_WORDS.map((word) => new RegExp(`\\b${word}\\b`, 'i'));
 
+// A very basic check for NSFW content that just looks for some keywords in the given
+// text
 function checkTextForNsfw(text) {
   const isNsfw = NSFW_REGEXES.some((regex) => regex.test(text));
 
